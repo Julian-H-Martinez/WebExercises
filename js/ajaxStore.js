01f5861511a95edf0e93ac74e5ad3c280bbf8791
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-04-23 16:21:18 -0500

    created js directory with js file for ajaxStore; connected with html

---
+++ js/ajaxStore.js
@@ -0,0 +1,34 @@
+(function(){
+    "use strict";
+    // TODO: Create an AJAX GET request for the file under data/inventory.json
+    const storeItems = $.ajax("data/inventory.json");
+
+    function showItem(item){
+        return (
+            '<tr><td>'
+            + item.title
+            + '</td><td>'
+            + item.quantity
+            + '</td><td>'
+            + item.price
+            + '</td><td>'
+            + item.categories
+            + '</td></tr>'
+        );
+    }
+
+    function onSuccess(data){
+        let displayItem = '';
+        data.forEach(function(item){
+            displayItem += showItem(item);
+        })
+        $('#insertProducts').append(displayItem);
+    }
+
+    // TODO: Take the data from inventory.json and append it to the products table
+    //       HINT: Your data should come back as a JSON object; use console.log() to inspect
+    //             its contents and fields
+    //       HINT: You will want to target #insertProducts for your new HTML elements
+
+    storeItems.done(onSuccess);
+})();
