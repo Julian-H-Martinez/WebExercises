20db135049073af9d942fcb88add0e0082befda7
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-04-25 11:34:20 -0500

    added some comments and cleaned up code a little more

--- js/ajaxStore.js
+++ js/ajaxStore.js
@@ -3,8 +3,11 @@
 
     const storeItems = $.ajax("data/inventory.json");
 
-    // storeItems.done(onSuccess);
+    storeItems
+        .done(onSuccess)
+        .error(onFailure);
 
+    //  function to show inventory in a table
     function showItem(item){
         return (
             '<tr><td>'
@@ -19,6 +22,7 @@
         );
     }
 
+    //  function for loading a page successfully
     function onSuccess(data){
         let displayItem = '';
         data.forEach(function(item){
@@ -27,9 +31,16 @@
         $('#insertProducts').append(displayItem);
     }
 
+    //  function for failure of loading a page
+    function onFailure(data){
+        alert("Error loading page; please check request")
+    }
+
     function getInventory(){
         storeItems
-            .done(onSuccess)
+            //  can use .success instead of done and .error instead of fail
+            .success(onSuccess)
+            .error(onFailure)
     }
 
     $('#refresh').click(function(e){
