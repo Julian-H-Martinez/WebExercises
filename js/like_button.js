c02465d4798b194993c3cc351af04892ee59db62
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-04-26 16:55:29 -0500

    added example code from React to follow along and practice

--- js/like_button.js
+++ js/like_button.js
@@ -26,37 +26,38 @@
 //     const root = ReactDOM.createRoot(domContainer);
 //     root.render(e(LikeButton));
 // })();
+(function(){
+    'use strict';
 
-'use strict';
+    const e = React.createElement;
 
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
-    }
-
-    render() {
-        if (this.state.liked) {
-            return 'You liked comment number ' + this.props.commentID;
+    class LikeButton extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = { liked: false };
         }
 
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
-        );
+        render() {
+            if (this.state.liked) {
+                return 'You liked comment number ' + this.props.commentID;
+            }
+
+            return e(
+                'button',
+                { onClick: () => this.setState({ liked: true }) },
+                'Like'
+            );
+        }
     }
-}
 
 // Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.like_button_container')
-    .forEach(domContainer => {
-        // Read the comment ID from a data-* attribute.
-        const commentID = parseInt(domContainer.dataset.commentid, 10);
-        ReactDOM.render(
-            e(LikeButton, { commentID: commentID }),
-            domContainer
-        );
-    });
+    document.querySelectorAll('.like_button_container')
+        .forEach(domContainer => {
+            // Read the comment ID from a data-* attribute.
+            const commentID = parseInt(domContainer.dataset.commentid, 10);
+            ReactDOM.render(
+                e(LikeButton, { commentID: commentID }),
+                domContainer
+            );
+        });
+})();
