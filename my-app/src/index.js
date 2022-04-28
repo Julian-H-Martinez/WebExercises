add859488558e05d200f15b1944ce466323ae747
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-04-28 14:03:08 -0500

    made Square into a function instead of a class; added default for square to be X otherwise O; game is still being worked on

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,32 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            value: null,
-        };
-    }
-    render() {
-        return (
-            // <button className="square" onClick={function(){console.log('click');}}>
-            // <button className="square" onClick={()=>console.log('click')}>
-            <button
-                className="square"
-                onClick = {() => this.setState({value:'X'})}
-            >
-                {/* TODO */}
-                {/*{this.props.value}*/}
-                {this.state.value}
-            </button>
-        );
-    }
+// class Square extends React.Component {
+//     // TODO: remove the constructor
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state={
+//     //         value: null,
+//     //     };
+//     // }
+//     render() {
+//         // TODO: use onClick={this.props.onClick}
+//         // TODO: replace this.state.value with this.props.value
+//         return (
+//             <button
+//                 className="square"
+//                 onClick = {() => this.props.onClick()}
+//                 // onClick = {() => this.setState({value:'X'})}
+//             >
+//                 {/* TODO */}
+//                 {this.props.value}
+//                 {/*{this.state.value}*/}
+//             </button>
+//         );
+//     }
+// }
+
+//  TODO: replace Square class to be a function
+function Square(props){
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
+    //  TODO: add constructor to Board
+    //  TODO: set Board's initial state to contain array of 9 nulls corresponding to the 9 squares
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            //  setting first move to be 'X' by default
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i){
+        //  creating a copy of the array
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        // return <Square value={i}/>;
+        return (
+            <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
