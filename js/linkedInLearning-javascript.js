38e5837d934b99e847e5b3a1ccb3c88a3cd3d442
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-05 11:28:29 -0500

    finished array lecture; next is manipulating arrays

--- js/linkedInLearning-javascript.js
+++ js/linkedInLearning-javascript.js
@@ -50,3 +50,44 @@ then reparse into an object
 animal2 = JSON.parse(JSON.stringify(animal));
  */
 
+/* ---ARRAYS--- */
+/*
+ARRAYS PRESERVES ORDER THAT IT'S BEEN ARRANGED AS
+ARRAYS HAVE KEYS THAT ARE NOT DISPLAYED (INDEX) AND
+AUTOMATICALLY ASSIGNED STARTING AT 0
+"SECRETLY" AN ARRAY IS AN OBJECT WITH SPECIAL TREATMENT
+ARRAYS CAN BE CREATED WITH ANY KIND OF DATA
+ARRAYS OFTEN USED FOR LISTS
+ARRAYS KNOW HOW MANY THINGS ARE IN THEM (.length)
+*/
+var myArray = [];
+// myArray = [0, 1, 2, 'string1', 'string2', 'string3', true, false]
+// console.log(myArray);
+// console.log(myArray.length);
+// console.log(typeof myArray[myArray.length-1]);
+
+var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// console.log(daysOfTheWeek);
+// console.log(typeof daysOfTheWeek);
+// console.log(daysOfTheWeek.length);
+
+var counties = [
+    'Bexar',
+    'Guadalupe',
+    'Comal',
+    'Gonzalez'
+]
+// console.log(counties);
+// console.log(counties.length);
+
+var arrayOfStuff = [
+    {'name': 'value'},
+    [1,2,3],
+    true,
+    420,
+    'This javascript stuff is pretty dope!'
+];
+// console.log(arrayOfStuff);
+// console.log(arrayOfStuff.length);
+// console.log(arrayOfStuff[1][2]);
+// console.log(arrayOfStuff[0].name);
