a02f53c2e7ce7cf205166f49fd9193d3fa72dcfd
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-11 14:10:09 -0500

    completed Objects, References, and Functions module; learned that a function that does not return anything modifies original object or array otherwise need to return a copy in order to not modify original

--- js/linkedInLearning-javascript.js
+++ js/linkedInLearning-javascript.js
@@ -514,6 +514,10 @@ closing curly brace '}'
 function someName(){ // do something // }
 
 functions can have as many arguments as you want
+
+when creating a function it's good to ensure that typeof argument being passed is what's required
+
+
  */
 //  BASIC FUNCTIONS
 console.log('Arf');
@@ -606,7 +610,90 @@ function addingMachine(){
     return total;
 }
 
+//  OBJECTS, REFERENCES, AND FUNCTIONS
 
+var calvin = {
+    name: "Calvin",
+    bestFriend: "Hobbes",
+    form: "human boy"
+};
+
+//  you can also pass an object to a function
+//  because objects are passed by reference, the argument will be modified
+//  function below will modify original object and not create a copy
+function transmogrifier(calvin){
+    if(typeof calvin !== 'object'){
+        console.error("argument is of the wrong type");
+        return;
+    }
+
+    //  generate a random number between 1 and 5
+    var randomNumber = Math.floor((Math.random()*5) + 1);
+
+    switch(randomNumber){
+        case 1:
+            calvin.form = 'tyrannosaurus';
+            break;
+        case 2:
+            calvin.form = 'grey wolf';
+            break;
+        case 3:
+            calvin.form = 'bengal tiger';
+            break;
+        case 4:
+            calvin.form = 'grizzly bear';
+            break;
+        case 5:
+            calvin.form = 'canary';
+            break;
+        default:
+            //  he stays human
+            calvin.form = 'human boy';
+            break;
+    }
+}
+
+function transmogrifierCopy(calvin){
+    if(typeof calvin !== 'object'){
+        console.log("argument is of the wrong type");
+        return;
+    }
+
+    //  generate a random number between 1 and 5
+    var randomNumber = Math.floor(Math.random()*5) + 1;
+    //  saving form as a variable so that we can modify the newForm and not original
+    var newForm = calvin.form;  //  by default, do not change
+
+    switch(randomNumber){
+        case 1:
+            newForm = 'tyrannosaurus';
+            break;
+        case 2:
+            newForm = 'grey wolf';
+            break;
+        case 3:
+            newForm = 'bengal tiger';
+            break;
+        case 4:
+            newForm = 'grizzly bear';
+            break;
+        case 5:
+            newForm = 'canary';
+            break;
+        default:
+            //  he stays human
+            newForm = 'human boy';
+            break;
+    }
+
+    //  return a new object that's just like calvin, but transmogrified
+    //  new object without modifying the old one
+    return {
+        name: calvin.name,
+        bestFriend: calvin.bestFriend,
+        form: newForm
+    };
+}
 
 
 
