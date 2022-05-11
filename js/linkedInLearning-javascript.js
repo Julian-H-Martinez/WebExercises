f8d79e2e22cfc6a678d732f5b6164515ac7e49c9
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-11 13:44:25 -0500

    completed More on Arguments module; learned about arguments array like object

--- js/linkedInLearning-javascript.js
+++ js/linkedInLearning-javascript.js
@@ -512,6 +512,8 @@ doing something followed by
 closing curly brace '}'
 
 function someName(){ // do something // }
+
+functions can have as many arguments as you want
  */
 //  BASIC FUNCTIONS
 console.log('Arf');
@@ -564,6 +566,45 @@ function isEven(num){
 isEven(44);
 isEven(45);
 
+//  MORE ON ARGUMENTS
+
+function speakSomething(what, howMany){
+    //  this pattern works nicely for default values
+    //  check if the argument is undefined, and if it is, provide a default value
+    var what = (typeof what !== 'undefined') ? what : "Default speech";
+    var howMany = (typeof howMany !== 'undefined') ? howMany : 10;
+
+    //  shorter version that could get tripped up by truthiness
+    /*
+    what = what || 'Default speech';
+    howMany = howMany || 10;
+     */
+
+    for(var i=0; i < howMany; i += 1){
+        console.log(what + " (" + i + ")");
+    }
+}
+
+function addingMachine(){
+    var total = 0;
+
+    //  keyword arguments is an object every function should know about
+    //  array like object that provides some of the array tools
+
+    for(var i = 0; i < arguments.length; i += 1){
+        //  grab the next number
+        var number = arguments[i];
+
+        //  check if the argument is a number
+        //  if so, add it to the running total
+        if(typeof number === 'number'){
+            total += number;
+        }
+    }
+
+    //  done - return total
+    return total;
+}
 
 
 
