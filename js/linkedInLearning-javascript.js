b9f9b19990432b3cd06ca409c8e2aa0b483801b6
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-12 10:10:23 -0500

    completed first 2 modules of chapter; asynchronous vs synchronous

--- js/linkedInLearning-javascript.js
+++ js/linkedInLearning-javascript.js
@@ -815,7 +815,86 @@ doubleIt = number => (number *= 2);
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 
+/* ---MORE ADVANCED PIECES--- */
 
+//  ASYNCHRONOUS CODE: THE WAITING IS THE HARDEST PART
+/*
+- code involving steps that can run at same time w/results of some steps coming later
+- some steps can be run in a way that doesn't block others
+- synchronous is step by step with previous step blocking next step
+- asynchronous comes from needing to load data from somewhere most likely the internet
+ */
+
+//  PROMISES, ASYNC, AND AWAIT
+
+
+
+
+/* Callbacks */
+
+// With one, it's simple enough
+jQuery.get("https://httpbin.org/get?data=1", function(response) {
+    // Now I have some data
+});
+
+// Callbacks get nested ad infinitum
+jQuery.get("https://httpbin.org/get", function(response) {
+    // Now I have some data
+
+    jQuery.get("https://httpbin.org/get", function(response) {
+        // Now I have some more data
+
+        jQuery.get("https://httpbin.org/get", function(response) {
+            // Now I have even more data!
+        });
+    });
+});
+
+/* Promises */
+
+// One Promise
+axios.get("https://httpbin.org/get")
+    .then(function(response)        //  .then() is the promise
+    {
+    // now I have some data
+});
+
+// Multiple promises in sequence, no nesting
+axios
+    .get("https://httpbin.org/get")
+    .then(function(response) {
+        // now I have some data
+
+        return axios.get("https://httpbin.org/get");
+    })
+    .then(function(response) {
+        // now I have some data
+
+        return axios.get("https://httpbin.org/get");
+    });
+
+/* Async / Await */
+/*
+- functions are marked as asynchronous w/keyword 'async'
+- network requests can have return values stored directly using keyword 'await' before promise
+*/
+
+// One request
+async function getOneThing() {
+    var response = await axios.get("https://httpbin.org/get");
+
+    // now I have some data
+}
+
+// Multiple requests
+//  each promise 'await's until ready
+async function getLotsOfThings() {
+    var response1 = await axios.get("https://httpbin.org/get");
+    var response2 = await axios.get("https://httpbin.org/get");
+    var response3 = await axios.get("https://httpbin.org/get");
+
+    // Now I have lots of data
+}
 
 
 
